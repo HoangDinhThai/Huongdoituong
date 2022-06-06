d189b5f13a2f4caad055688abd120c1e21880045
HoangDinhThai <100067398+HoangDinhThai@users.noreply.github.com>
2022-06-06 11:50:40 +0700

    Add files via upload

---
+++ node.java
@@ -0,0 +1,34 @@
+public class node {
+
+    private int data;
+    private node next;
+
+    public node() {
+    }
+
+    public node(int data, node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public node getNext() {
+        return next;
+    }
+
+    public void setNext(node next) {
+        this.next = next;
+    }
+}
