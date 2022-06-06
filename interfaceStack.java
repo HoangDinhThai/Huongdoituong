741457f6bf5870eb4027c7dbe66fae4c9e19f727
HoangDinhThai <100067398+HoangDinhThai@users.noreply.github.com>
2022-06-06 11:49:05 +0700

    Add files via upload

---
+++ interfaceStack.java
@@ -0,0 +1,12 @@
+public interface interfaceStack {
+
+    public void push(int data);
+
+    public int pop();
+
+    public int top();
+
+    public boolean isEmpty();
+
+    public boolean isFull();
+}
